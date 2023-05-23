8b33d94f675276d223f9c6a2d087e6ea601ddf5a
George <81578359+georgy-p@users.noreply.github.com>
2023-05-23 21:36:36 +0300

    add Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
+import styled from 'styled-components';
+import styles from './styles/index';
+import Background from './components/background/Background';
+
+
+const Wrapper = styled.section`
+  margin: 0rem 7rem 0rem 14rem;
+  height: 100vh;
+  border: 0.5px solid;
+  border-color: ${styles.colors.grey}
+`;
 
 const App = () => {
-  return <HelloWorld />;
+  return (
+  <Wrapper>
+    <Background />
+  </Wrapper>
+  )
 };
 
 export default App;
