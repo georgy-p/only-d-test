1c5d9799278f0b6ce6cf42bb0a65c467cac659ae
George <81578359+georgy-p@users.noreply.github.com>
2023-05-23 21:38:55 +0300

    create main styles variables

---
+++ src/styles/index.tsx
@@ -0,0 +1,19 @@
+const styles = {
+  colors: {
+    white: '#F4F5F9',
+    grey: 'rgb(226, 229, 235)',
+    dark_blue: '#42567A',
+    blue: '#5D5FEF',
+    pink: '#EF5DA8'
+  },
+  fonts: {
+    main: `'PT Sans', sans-serif`,
+    extra: `'Bebas Neue', sans-serif`,
+    desktop_size: '20px',
+    mobile_size: '14px',
+    weight_regular: '400',
+    weight_bold: '700',
+  }
+};
+
+export default styles;
