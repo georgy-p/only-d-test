e7237b3e3db70337f01c1caee29fc67da6eb0702
Георгий Пигарь <georgy-p@MacBook-Pro-Georgij.local>
2023-05-26 18:08:47 +0300

    separate Circle to another component

---
+++ src/components/CircleControl.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import styles from '../styles';
+
+const CircleContainer = styled.div`
+  width: 450px;
+  height: 450px;
+
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
+
+const CircleStyled = styled.div`
+  z-index: 10;
+  height: 100%;
+  width: 100%;
+  border: solid;
+  border-radius: 50%;
+  border-color: ${styles.colors.dark_grey};
+`;
+
+const CircleControl = () => {
+  return (
+    <CircleContainer>
+      <CircleStyled />
+    </CircleContainer>
+  );
+};
+
+export default CircleControl;
--- src/components/background/Background.tsx
+++ src/components/background/Background.tsx
@@ -6,13 +6,16 @@ const Border = styled.div`
   border: 0.5px solid;
   border-color: ${styles.colors.grey};
   height: 100%;
+
+  @media (max-width: 1000px) {
+    display: none;
+  }
 `;
 
 const HorizontalLine = styled.div`
   position: absolute;
   left: 0;
-  right: 0;
-  top: 50%;
+  top: 45%;
   transform: translateY(-50%);
   height: 1px;
   background-color: ${styles.colors.grey};
@@ -28,33 +31,11 @@ const VerticalLine = styled.div`
   background-color: ${styles.colors.grey};
 `;
 
-const CircleContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const CircleStyled = styled.div`
-  z-index: 10;
-  height: 25rem;
-  width: 25rem;
-  border: solid;
-  border-radius: 50%;
-  border-color: ${styles.colors.dark_grey};
-  text-align: center;
-`;
-
 const Background = () => {
   return (
     <Border>
       <HorizontalLine />
       <VerticalLine />
-      <CircleContainer>
-        <CircleStyled />
-      </CircleContainer>
     </Border>
   );
 };
